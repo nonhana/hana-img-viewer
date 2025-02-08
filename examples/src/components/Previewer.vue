067efa344e18abb93c2a26b45f6b8221435b7807
non_hana <1209220829@qq.com>
2025-02-08 16:36:18 +0800

    feat: add github link

--- examples/src/components/Previewer.vue
+++ examples/src/components/Previewer.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import demoImg2 from '../assets/114388636.jpg'
 import demoImg1 from '../assets/116811075-1.jpg'
+import GithubSVG from './GithubSVG.vue'
 </script>
 
 <template>
   <div class="wrapper">
+    <a class="icon" href="https://github.com/nonhana/hana-img-viewer" target="_blank">
+      <GithubSVG />
+    </a>
     <hana-img-viewer class="image" :src="demoImg1" :alt="demoImg1" />
     <hana-img-viewer class="image" :src="demoImg2" :alt="demoImg2" />
   </div>
@@ -17,6 +21,12 @@ import demoImg1 from '../assets/116811075-1.jpg'
   align-items: center;
   flex-direction: column;
   gap: 32px;
+  .icon {
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 16px;
+  }
   @media (min-width: 820px) {
     flex-direction: row;
     justify-content: space-between;
