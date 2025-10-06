1cc2ab09379377a8c6e6e6d2bcb149f28bf6ca53
non_hana <nonhana@outlook.com>
2025-10-07 00:26:10 +0800

    chore: optimize dialog title

--- examples/src/components/Dialog.vue
+++ examples/src/components/Dialog.vue
@@ -13,7 +13,7 @@ function open() {
   <el-button @click="open">
     弹窗内图片预览
   </el-button>
-  <el-dialog v-model="visible" title="Dialog" width="30%">
+  <el-dialog v-model="visible" title="请滚动到底部查看内嵌效果" width="30%">
     <div style="height: 2000px; width: 100%;" />
     <HanaImgViewer src="https://pixiv-r2.caelum.moe/121909597.png" alt="121909597.png" />
   </el-dialog>
